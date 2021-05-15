2dcc7d973b3daff814ddf4d3fe002cfe02231c97
nameIsErik <75945657+nameIsErik@users.noreply.github.com>
2021-05-15 16:27:32 +0300

    Initial project

---
+++ AnimeChest/AnimeChest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeChest
+//
+//  Created by Ольга Ерохина on 5/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
