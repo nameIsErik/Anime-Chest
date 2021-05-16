467e43cfe55421a4c01076b05e808a05b6cb934b
nameIsErik <75945657+nameIsErik@users.noreply.github.com>
2021-05-16 23:06:00 +0300

    Added Login Screen
    Login screen with segue to collection

---
+++ AnimeChest/AnimeChest/Controllers/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  AnimeChest
+//
+//  Created by Erik Kokaev on 5/16/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.tintColor = UIColor.gray.withAlphaComponent(1)
+
+        errorLabel.alpha = 0
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(loginButton)
+        Utilities.styleTextFieldPlaceholder(emailTextField, text: "Email")
+        Utilities.styleTextFieldPlaceholder(passwordTextField, text: "Password")
+    }
+    
+
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, err) in
+            
+            if err != nil {
+                self.errorLabel.text = err!.localizedDescription
+                self.errorLabel.alpha = 1
+            } else {
+                self.transitionToCollection()
+            }
+        }
+    }
+   
+    
+    func transitionToCollection() {
+        performSegue(withIdentifier: Constants.Segues.LoginToCollection, sender: nil)
+    }
+}
---
+++ AnimeChest/AnimeChest/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AnimeChest
+//
+//  Created by Erik Kokaev on 5/16/21.
+//
+
+
+struct Constants {
+    
+    struct Segues {
+      static let LoginToCollection = "LoginToCollection"
+    }
+}
