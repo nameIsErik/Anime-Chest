8fd5f2884ae10467f4c76d10f197e9f6ed496192
nameIsErik <75945657+nameIsErik@users.noreply.github.com>
2021-05-18 00:22:41 +0300

    Added AnimeItem entity
    Init from Firebase Snapshot, self init, and method to convert to ANY

---
+++ AnimeChest/AnimeChest/Model/AnimeItem.swift
@@ -0,0 +1,64 @@
+//
+//  AnimeItem.swift
+//  AnimeChest
+//
+//  Created by Erik Kokaev on 5/17/21.
+//
+
+import Firebase
+
+
+class Animeitem {
+    
+    let ref: DatabaseReference?
+    var title: String
+    var episodes: Int
+    var video: String
+    var imagesURLs: [String]
+    var mapX: Double
+    var mapY: Double
+    
+    init(title: String, episodes: Int, video: String, imagesURLs: [String], mapX: Double, mapY: Double) {
+        
+        self.ref = nil
+        self.title = title
+        self.episodes = episodes
+        self.video = video
+        self.imagesURLs = imagesURLs
+        self.mapX = mapX
+        self.mapY = mapY
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard
+            let value = snapshot.value as? [String: AnyObject],
+            let title = value["title"] as? String,
+            let episodes = value["episodes"] as? Int,
+            let video = value["video"] as? String,
+            let imagesURLs = value["imagesURLs"] as? [String],
+            let mapX = value["mapX"] as? Double,
+            let mapY = value["mapY"] as? Double else {
+            return nil
+        }
+        
+        self.ref = snapshot.ref
+        self.title = title
+        self.episodes = episodes
+        self.video = video
+        self.imagesURLs = imagesURLs
+        self.mapX = mapX
+        self.mapY = mapY
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "title": title,
+            "episodes": episodes,
+            "video": video,
+            "imagesURLs": imagesURLs,
+            "mapX": mapX,
+            "mapY": mapY
+        ]
+    }
+
+}
